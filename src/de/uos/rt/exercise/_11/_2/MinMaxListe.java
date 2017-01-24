6bb2f4c5e7d1ef946384342ba8e134c01da9f3a2
speedcoder96 <rene.t.sommerfeld@gmail.com>
2017-01-24 21:21:32 +0100

    MinMaxListe kommentiert

--- src/de/uos/rt/exercise/_11/_2/MinMaxListe.java
+++ src/de/uos/rt/exercise/_11/_2/MinMaxListe.java
@@ -8,16 +8,21 @@ import de.uos.rt.exercise._10.VerweisListe;
  */
 public class MinMaxListe extends VerweisListe {
 
+	//das Minimum-Element
 	private Object minElem;
+	//das Maximum-Element
 	private Object maxElem;
 	
+	/**
+	 * MinMaxListe Konstruktor
+	 */
 	public MinMaxListe() {
 		minElem = null;
 		maxElem = null;
 	}
 	
 	/**
-	 * 
+	 * Fuegt ein neues Element der Liste hinzu
 	 */
 	public void insert(Object x) {
 		super.insert(x);
@@ -25,34 +30,42 @@ public class MinMaxListe extends VerweisListe {
 	}
 
 	/**
-	 *
+	 * Loescht das aktuelle Element aus der Liste
 	 */
 	public void delete() {
+		//aktuelles Element zwischenspeichern
 		Object currentElement = elem();
 		super.delete();
 		
 		if(empty()) {
+			//Speicher aufraeumen
 			minElem = maxElem = null;
+			//wenn das entfernte Element das Minimum oder Maximum war
 		} else if(currentElement == minElem || currentElement == maxElem) {
+			//aktuelle Position der Liste zwischenspeichern
 			if(!endpos()) {
 				currentElement = elem();
 			}
 			reset();
 			
+			//Annahme das das Maximum das erste Element der Liste ist
 			minElem = maxElem = elem();
 			
 			boolean setToDefault = false;
 			boolean resetList = false;
 			
 			while(!setToDefault) {
+				//neues Minimum und Maximum finden
 				if(!resetList) {
 					if(!endpos()) {
 						compareToMinMax(elem());
 						advance();
 					} else {
+						//Liste zuruecksetzen
 						resetList = true;
 						reset();
 					}
+					//Liste wieder zur urspruenglichen Position zuruecksetzen
 				} else {
 					if(!endpos() && currentElement != elem()) {
 						advance();
@@ -65,24 +78,36 @@ public class MinMaxListe extends VerweisListe {
 	}
 	
 	/**
-	 * 
-	 * @param current
+	 * Vergleicht das <tt> current </tt> mit dem
+	 * Minimum und dem Maximum aus dieser Klasse und
+	 * setzt diese neu.
+	 * @param current das Element das mit Minimum und Maximum
+	 * vergleicht werden soll
 	 */
 	private void compareToMinMax(Object current) {
 		Comparable currentElement = (Comparable)current;
+		//vergleicht das Element mit dem Minimum
 		if(minElem == null || currentElement.compareTo(minElem) < 0) {
 			minElem = current;
 		} 
+		//vergleicht das Element mit dem Maximum
 		if(maxElem == null || currentElement.compareTo(maxElem) > 0) {
 			maxElem = current;
 		}
 	}
 	
-	
+	/**
+	 * Gibt das aktuelle Minimum zurueck
+	 * @return das aktuelle Minimum
+	 */
 	public Object getMin() {
 		return minElem;
 	}
 	
+	/**
+	 * Gibt das aktuelle Maximum zurueck
+	 * @return das aktuelle Maximum
+	 */
 	public Object getMax() {
 		return maxElem;
 	}
